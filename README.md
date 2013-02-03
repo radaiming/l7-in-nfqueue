38385ec764879dde9833c205e7535d4425934f4d
Ming Dai <radaiming@gmail.com>
2013-02-03 20:44:59 +0800

    clean all the four items in TODO list

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 As [l7](http://l7-filter.clearfoundation.com/start) for iptables is not updated for a long time,and I don't like to patch both kernel and iptables,so I try to write this script,which reads l7 pattern file and apply to layer 7 payloads,then decide whether to drop or accept.
 
-This script is not tested widely at the moment,but at least could read qq.pat and forbid qq from logining.
+This script is not and maybe won't be tested widely.
 
 ## Usage:
 
@@ -12,11 +12,19 @@ First install [NetfilterQueue](http://pypi.python.org/pypi/NetfilterQueue),a pyt
 
 then use iptables' NFQUEUE to feed the script with certain packets,for example:
 
-\# iptablles -A FORWARD -p tcp -j NFQUEUE --queue-num 1
+\# iptables -A FORWARD -p tcp -j NFQUEUE --queue-num 1
+\# iptables -A FORWARD -p udp -j NFQUEUE --queue-num 1
+
+There are some limitations in regex for l7 kernel version,if you want to use regex similar to userspace version(the regex you may be more familliar with,see more in [pattern writing howto](http://l7-filter.clearfoundation.com/docs/pattern_writing_howto)),add -P option:
+
+\# ./l7-in-nfqueue.py -P /path/to/your/l7_pattern_file
+
+When package dropped,some information of the package will be printed,like this:
+packet dropped,from 127.0.0.1:7070 to 127.0.0.1:55023 with length 79
 
 ## TODO
 
-* Seems I need to get more familiar with l7 patterns,at the moment script could only parse pattern files with only one pattern
-* Maybe I should support more than TCP and UDP?And also TCP and UDP header length are not always fixed,this should be considered.
-* Print more information when dropping packets
-* The most important:I shouldn't be so lazy,please clean TODOs above in one month(orz...).
+* ~~Seems I need to get more familiar with l7 patterns,at the moment script could only parse pattern files with only one pattern~~(seems familliar now?)
+* ~~Maybe I should support more than TCP and UDP?And also TCP and UDP header length are not always fixed,this should be considered~~(supporting for TCP and UDP seems enough at the moment.IP and TCP headers' length are read from headers now.)
+* ~~Print more information when dropping packets~~(Done)
+* Read multiple pattern files once.(maybe won't be done because I'm too lazy)
--- l7-in-nfqueue.py
+++ l7-in-nfqueue.py
@@ -6,31 +6,76 @@ import os
 import re
 from netfilterqueue import NetfilterQueue
 
-if len(sys.argv) == 1:
-    print 'usage: ' + os.path.basename(sys.argv[0]) + ' path-to-protocol-config-file'
+def print_err_msg():
+    print 'if you want to use l7 pattern file:' + os.path.basename(sys.argv[0]) + ' /path/to/your/l7_pattern_file'
+    print 'if you want to use regex to search payload in layer 7,add -P'
     exit(1)
-config_file = open(sys.argv[1],'r')
-pattern = ''
-for line in config_file:
-    if not line.startswith('#') and line.strip('\n') != os.path.basename(sys.argv[1])[0:-4] and len(line):
-        pattern = line.strip('\n')
 
-def print_and_accept(pkt):
+def print_pkt_drop_info(ip_payload,ip_header_length,pkt_length):
+    src_addr = str(ord(ip_payload[12])) + '.' + str(ord(ip_payload[13])) + '.' + str(ord(ip_payload[14])) + '.' + str(ord(ip_payload[15]))
+    dst_addr = str(ord(ip_payload[16])) + '.' + str(ord(ip_payload[17])) + '.' + str(ord(ip_payload[18])) + '.' + str(ord(ip_payload[19]))
+    src_port = str(int(hex(ord(ip_payload[ip_header_length:ip_header_length + 1]))[2:] + hex(ord(ip_payload[ip_header_length + 1:ip_header_length + 2]))[2:],16))
+    dst_port = str(int(hex(ord(ip_payload[ip_header_length + 2:ip_header_length + 3]))[2:] + hex(ord(ip_payload[ip_header_length + 3:ip_header_length + 4]))[2:],16))
+    print 'packet dropped,from ' + (src_addr + ':' + src_port).ljust(21) + ' to ' + (dst_addr + ':' + dst_port).ljust(21) + ' with length ' + str(pkt_length)
+
+def pkt_check(pkt):
     ip_payload = pkt.get_payload()
-    if ip_payload[9] == '\x06':                 # tcp
-        layer7_payload = ip_payload[40:]
-    elif ip_payload[9] == '\x11':               # udp
-        layer7_payload = ip_payload[28:]
+    ip_header_length = (ord(ip_payload[0]) & 15)*4
+    if ip_payload[9] == '\x06':                 ####### tcp ####### 
+        tcp_header_length = ((ord(ip_payload[ip_header_length + 13 - 1]) & 240) >> 4)*4
+        layer7_payload = ip_payload[ip_header_length + tcp_header_length:]
+    elif ip_payload[9] == '\x11':               ####### udp ####### 
+        udp_header_length = 8
+        layer7_payload = ip_payload[ip_header_length + udp_header_length:]
 
-    if re.match(pattern,layer7_payload):
-        print 'package droped,length ' + str(len(layer7_payload))
-        pkt.drop()
+    if regex:
+        if re.search(pattern,layer7_payload):
+            print_pkt_drop_info(ip_payload,ip_header_length,pkt.get_payload_len())
+            pkt.drop()
+        else:
+            pkt.accept()
     else:
-        #print 'package accepted,length ' + str(len(layer7_payload))
-        pkt.accept()
+        if re.search(pattern,layer7_payload.replace('\x00',''),flags=re.IGNORECASE):
+            print_pkt_drop_info(ip_payload,ip_header_length,pkt.get_payload_len())
+            pkt.drop()
+        else:
+            pkt.accept()
 
+####### read arguments #######
+if len(sys.argv) == 3:
+    if sys.argv[1].startswith('-'):
+        argument = sys.argv[1]
+        path_to_pattern_file = sys.argv[2]
+    elif sys.argv[2].startswith('-'):
+        argument = sys.argv[2]
+        path_to_pattern_file = sys.argv[1]
+    else:
+        print_err_msg()
+elif len(sys.argv) == 2:
+    argument = ''
+    path_to_pattern_file = sys.argv[1]
+else:
+    print_err_msg()
+
+####### check argument #######
+if argument == '-P':
+    regex = 1
+else:
+    regex = 0
+if not os.path.exists(path_to_pattern_file):
+    print 'file ' + path_to_pattern_file + ' does not exist'
+    exit(1)
+
+####### read l7 pattern file #######
+pattern_file = open(path_to_pattern_file,'r')
+for line in pattern_file:
+    if not line.startswith('#') and not line.startswith('userspace') and line.strip('\n') != os.path.basename(sys.argv[1])[0:-4] and len(line):
+        pattern = line.strip('\n')
+pattern_file.close()
+
+####### now start checking packets from nfqueue #######
 nfqueue = NetfilterQueue()
-nfqueue.bind(1, print_and_accept)
+nfqueue.bind(1, pkt_check)
 try:
     nfqueue.run()
 except KeyboardInterrupt:
