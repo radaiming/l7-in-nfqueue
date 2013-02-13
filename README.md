b41c68b90bd84a40f616909bbeb4e5267f691f44
Ming Dai <radaiming@gmail.com>
2013-02-13 14:58:10 +0800

    now could read multiple pattern files

--- README.md
+++ README.md
@@ -1,30 +1,34 @@
 # l7-in-nfqueue
 
-As [l7](http://l7-filter.clearfoundation.com/start) for iptables is not updated for a long time,and I don't like to patch both kernel and iptables,so I try to write this script,which reads l7 pattern file and apply to layer 7 payloads,then decide whether to drop or accept.
+As [l7](http://l7-filter.clearfoundation.com/start) for iptables is not updated for a long time, and I don't like to patch both kernel and iptables, so I try to write this script, which reads l7 pattern file and apply to layer 7 payloads, then decide whether to drop or accept.
 
 This script is not and maybe won't be tested widely.
 
 ## Usage:
 
-First install [NetfilterQueue](http://pypi.python.org/pypi/NetfilterQueue),a python bindings for libnetfilter_queue,then run this script as root:
+First install [NetfilterQueue](http://pypi.python.org/pypi/NetfilterQueue), a python bindings for libnetfilter_queue, then run this script as root:
 
 \# ./l7-in-nfqueue.py /path/to/your/l7_pattern_file
 
-then use iptables' NFQUEUE to feed the script with certain packets,for example:
+or read multiple pattern files one time:
+
+\# ./l7-in-nfqueue.py /path/to/your/l7_pattern_file01 /path/to/your/l7_pattern_file02
+
+then use iptables' NFQUEUE to feed the script with certain packets, for example:
 
 \# iptables -A FORWARD -p tcp -j NFQUEUE --queue-num 1
 \# iptables -A FORWARD -p udp -j NFQUEUE --queue-num 1
 
-There are some limitations in regex for l7 kernel version,if you want to use regex similar to userspace version(the regex you may be more familliar with,see more in [pattern writing howto](http://l7-filter.clearfoundation.com/docs/pattern_writing_howto)),add -P option:
+There are some limitations in regex for l7 kernel version, if you want to use regex similar to userspace version(the regex you may be more familliar with, see more in [pattern writing howto](http://l7-filter.clearfoundation.com/docs/pattern_writing_howto)), add -e or -P option(they are the same):
 
-\# ./l7-in-nfqueue.py -P /path/to/your/l7_pattern_file
+\# ./l7-in-nfqueue.py -e /path/to/your/l7_pattern_file
 
-When package dropped,some information of the package will be printed,like this:
-packet dropped,from 127.0.0.1:7070 to 127.0.0.1:55023 with length 79
+When package dropped, some information of the package will be printed, like this:
+packet dropped: 127.0.0.1:7070        --> 127.0.0.1:55186       | 433  bytes
 
 ## TODO
 
-* ~~Seems I need to get more familiar with l7 patterns,at the moment script could only parse pattern files with only one pattern~~(seems familliar now?)
-* ~~Maybe I should support more than TCP and UDP?And also TCP and UDP header length are not always fixed,this should be considered~~(supporting for TCP and UDP seems enough at the moment.IP and TCP headers' length are read from headers now.)
+* ~~Seems I need to get more familiar with l7 patterns, at the moment script could only parse pattern files with only one pattern~~(seems familliar now?)
+* ~~Maybe I should support more than TCP and UDP?And also TCP and UDP header length are not always fixed, this should be considered~~(supporting for TCP and UDP seems enough at the moment.IP and TCP headers' length are read from headers now)
 * ~~Print more information when dropping packets~~(Done)
-* Read multiple pattern files once.(maybe won't be done because I'm too lazy)
+* ~~Read multiple pattern files once~~(Done)
--- l7-in-nfqueue.py
+++ l7-in-nfqueue.py
@@ -6,7 +6,12 @@ import os
 import re
 from netfilterqueue import NetfilterQueue
 
-def print_err_msg():
+###### seems could not pass argument to pkt_check in nfqueue.bind(1, pkt_check),so use some global variables ######
+global regex
+global patterns
+patterns = []
+
+def print_parse_err_msg():
     print 'if you want to use l7 pattern file:' + os.path.basename(sys.argv[0]) + ' /path/to/your/l7_pattern_file'
     print 'if you want to use regex to search payload in layer 7,add -P'
     exit(1)
@@ -16,9 +21,12 @@ def print_pkt_drop_info(ip_payload,ip_header_length,pkt_length):
     dst_addr = str(ord(ip_payload[16])) + '.' + str(ord(ip_payload[17])) + '.' + str(ord(ip_payload[18])) + '.' + str(ord(ip_payload[19]))
     src_port = str(int(hex(ord(ip_payload[ip_header_length:ip_header_length + 1]))[2:] + hex(ord(ip_payload[ip_header_length + 1:ip_header_length + 2]))[2:],16))
     dst_port = str(int(hex(ord(ip_payload[ip_header_length + 2:ip_header_length + 3]))[2:] + hex(ord(ip_payload[ip_header_length + 3:ip_header_length + 4]))[2:],16))
-    print 'packet dropped,from ' + (src_addr + ':' + src_port).ljust(21) + ' to ' + (dst_addr + ':' + dst_port).ljust(21) + ' with length ' + str(pkt_length)
+    #print 'packet dropped,from  ' + (src_addr + ':' + src_port).ljust(21) + '  to  ' + (dst_addr + ':' + dst_port).ljust(21) + '  with length ' + str(pkt_length).ljust(4) + ' bytes'
+    print 'packet dropped: ' + (src_addr + ':' + src_port).ljust(21) + ' --> ' + (dst_addr + ':' + dst_port).ljust(21) + ' | ' + str(pkt_length).ljust(4) + ' bytes'
 
 def pkt_check(pkt):
+    global regex
+    global patterns
     ip_payload = pkt.get_payload()
     ip_header_length = (ord(ip_payload[0]) & 15)*4
     if ip_payload[9] == '\x06':                 ####### tcp ####### 
@@ -28,56 +36,66 @@ def pkt_check(pkt):
         udp_header_length = 8
         layer7_payload = ip_payload[ip_header_length + udp_header_length:]
 
+    ACCEPT_THIS_PKT = True
     if regex:
-        if re.search(pattern,layer7_payload):
-            print_pkt_drop_info(ip_payload,ip_header_length,pkt.get_payload_len())
-            pkt.drop()
-        else:
+        for pattern in patterns:
+            if re.search(pattern,layer7_payload):
+                print_pkt_drop_info(ip_payload,ip_header_length,pkt.get_payload_len())
+                pkt.drop()
+                ACCEPT_THIS_PKT = False
+                break
+        if ACCEPT_THIS_PKT:
             pkt.accept()
     else:
-        if re.search(pattern,layer7_payload.replace('\x00',''),flags=re.IGNORECASE):
-            print_pkt_drop_info(ip_payload,ip_header_length,pkt.get_payload_len())
-            pkt.drop()
-        else:
+        for pattern in patterns:
+            if re.search(pattern,layer7_payload.replace('\x00',''),flags=re.IGNORECASE):
+                print_pkt_drop_info(ip_payload,ip_header_length,pkt.get_payload_len())
+                pkt.drop()
+                ACCEPT_THIS_PKT = False
+                break
+        if ACCEPT_THIS_PKT:
             pkt.accept()
 
-####### read arguments #######
-if len(sys.argv) == 3:
-    if sys.argv[1].startswith('-'):
-        argument = sys.argv[1]
-        path_to_pattern_file = sys.argv[2]
-    elif sys.argv[2].startswith('-'):
-        argument = sys.argv[2]
-        path_to_pattern_file = sys.argv[1]
+def parse_arg_and_patterns():
+    global regex
+    global patterns
+    ####### read & check arguments #######
+
+    path_to_pattern_files = list(sys.argv)[1:]
+    if len(path_to_pattern_files) < 2:
+        print_parse_err_msg()
+    if '-P' in path_to_pattern_files:
+        regex = True
+        path_to_pattern_files.remove('-P')
+    elif '-e' in path_to_pattern_files:
+        regex = True
+        path_to_pattern_files.remove('-e')
     else:
-        print_err_msg()
-elif len(sys.argv) == 2:
-    argument = ''
-    path_to_pattern_file = sys.argv[1]
-else:
-    print_err_msg()
+        regex = False
 
-####### check argument #######
-if argument == '-P':
-    regex = 1
-else:
-    regex = 0
-if not os.path.exists(path_to_pattern_file):
-    print 'file ' + path_to_pattern_file + ' does not exist'
-    exit(1)
+    for path_to_pattern_file in path_to_pattern_files:
+        if not os.path.exists(path_to_pattern_file):
+            print 'file ' + path_to_pattern_file + ' does not exist'
+            exit(1)
+    
+    ####### read l7 pattern file #######
+    for path_to_pattern_file in path_to_pattern_files:
+        pattern_file = open(path_to_pattern_file,'r')
+        for line in pattern_file:
+            if not line.startswith('#') and not line.startswith('userspace') and line.strip() != os.path.basename(path_to_pattern_file)[0:-4] and len(line.strip()):
+                patterns.append(line.strip())
+        pattern_file.close()
 
-####### read l7 pattern file #######
-pattern_file = open(path_to_pattern_file,'r')
-for line in pattern_file:
-    if not line.startswith('#') and not line.startswith('userspace') and line.strip('\n') != os.path.basename(sys.argv[1])[0:-4] and len(line):
-        pattern = line.strip('\n')
-pattern_file.close()
+def main():
+    parse_arg_and_patterns()
+    nfqueue = NetfilterQueue()
+    nfqueue.bind(1, pkt_check)
+    try:
+        nfqueue.run()
+    except KeyboardInterrupt:
+        print
+    finally:
+        nfqueue.unbind()
 
-####### now start checking packets from nfqueue #######
-nfqueue = NetfilterQueue()
-nfqueue.bind(1, pkt_check)
-try:
-    nfqueue.run()
-except KeyboardInterrupt:
-    print
-nfqueue.unbind()
+if __name__ == '__main__':
+    main()
